da57dac9e1b039cd61127244e106b462a65254c5
ovidiujordache <d00187472@student.dkit.ie>
2023-11-06 05:00:20 +0000

    navbar added

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Text.Json;
+using MongoDB.Driver;
+using MongoDB.Bson;
 
 namespace SoaCA1.Pages
 {
@@ -14,6 +17,24 @@ namespace SoaCA1.Pages
 
         public void OnGet()
         {
+         
+
+       /*     var connectionString = Environment.GetEnvironmentVariable("MONGODB_URI");
+            if (connectionString == null)
+            {
+                Console.WriteLine("You must set your 'MONGODB_URI' environment variable. To learn how to set it, see https://www.mongodb.com/docs/drivers/csharp/current/quick-start/#set-your-connection-string");
+                Environment.Exit(0);
+            }
+
+            var client = new MongoClient(connectionString);
+
+            var collection = client.GetDatabase("sample_mflix").GetCollection<BsonDocument>("movies");
+
+            var filter = Builders<BsonDocument>.Filter.Eq("title", "Back to the Future");
+
+            var document = collection.Find(filter).First();
+
+            Console.WriteLine(document);*/
 
         }
     }
